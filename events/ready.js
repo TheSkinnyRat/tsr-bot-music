d7816d0f5ad415d28da6e662400e4f76cdd4b263
TheSkinnyRat <purwa.sabrang@gmail.com>
2022-04-23 23:02:48 +0700

    presence display current version

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,15 @@
 module.exports = async (client) => {
   (client.Ready = true),
     client.user.setPresence({
-    status: client.botconfig.Presence.status, // You can show online, idle, and dnd
+      status: client.botconfig.Presence.status, // You can show online, idle, and dnd
       activity: {
-        name: client.botconfig.Presence.name,
+        name:
+          client.botconfig.Presence.name === "version"
+            ? `v${require("../package.json").version}`
+            : client.botconfig.Presence.name, // The message shown | version
         type: client.botconfig.Presence.type,
       },
-});
+    });
   client.Manager.init(client.user.id);
   client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
   client.RegisterSlashCommands();
