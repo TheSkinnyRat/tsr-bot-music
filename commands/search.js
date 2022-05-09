3883166293fd63d83d9926ad02d5f605de3dc8cb
TheSkinnyRat <purwa.sabrang@gmail.com>
2022-05-09 09:58:04 +0700

    change `search` to display more compact

--- commands/search.js
+++ commands/search.js
@@ -61,22 +61,21 @@ module.exports = {
     if (Searched.loadType == "NO_MATCHES")
       return client.sendTime(
         message.channel,
-        "No matches found for " + SearchString
+        "No matches found for - " + SearchString
       );
     else {
       Searched.tracks = Searched.tracks.map((s, i) => {
         s.index = i;
         return s;
       });
-      let songs = _.chunk(Searched.tracks, 10);
+      let songs = _.chunk(Searched.tracks, 5);
       let Pages = songs.map((songz) => {
         let MappedSongs = songz.map(
           (s) =>
-            `\`${s.index + 1}.\` [${s.title}](${
-              s.uri
-            }) \nDuration: \`${prettyMilliseconds(s.duration, {
-              colonNotation: true,
-            })}\``
+            `\`${s.index + 1}.\` [${s.title}](${s.uri}) (${prettyMilliseconds(
+              s.duration,
+              { colonNotation: true }
+            )})`
         );
 
         let em = new MessageEmbed()
@@ -85,7 +84,7 @@ module.exports = {
             client.botconfig.IconURL
           )
           .setColor(client.botconfig.EmbedColor)
-          .setDescription(MappedSongs.join("\n\n"));
+          .setDescription(MappedSongs.join("\n"));
         return em;
       });
 
