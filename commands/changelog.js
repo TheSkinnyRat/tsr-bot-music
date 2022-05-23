4cb5e70fec3819abdd7e8bf91195b7b24e7ee70f
TheSkinnyRat <purwa.sabrang@gmail.com>
2022-05-23 12:28:02 +0700

    Update `changelog` command

--- commands/changelog.js
+++ commands/changelog.js
@@ -5,12 +5,12 @@ const path = require("path");
 module.exports = {
   name: "changelog",
   description: "Show updates changelog",
-  usage: "",
+  usage: "<all>",
   permissions: {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
     member: [],
   },
-  aliases: [],
+  aliases: ["chlog", "updates", "update"],
   /**
    *
    * @param {import("../structures/DiscordMusicBot")} client
@@ -19,6 +19,38 @@ module.exports = {
    * @param {*} param3
    */
   run: async (client, message, args, { GuildDB }) => {
+    if (args[0]) {
+      if (args[0] == "all") {
+        try {
+          const changeLogData = fs.readFileSync(
+            path.join(__dirname, "..", "changelog.md")
+          );
+          return message.channel.send({
+            files: [
+              {
+                attachment: changeLogData || "No changelog found",
+                name: "changelog.txt",
+              },
+            ],
+          });
+        } catch (e) {
+          let ChangelogEmbed = new MessageEmbed()
+            .setColor("RED")
+            .setDescription("Error when load changelog file");
+          return message.channel.send(ChangelogEmbed);
+        }
+      }
+      let ChangelogEmbed = new MessageEmbed()
+        .setColor(client.botconfig.EmbedColor)
+        .setDescription(`Cannot find option for \`${args[0]}\`.`)
+        .setFooter(
+          `Type \`${
+            GuildDB ? GuildDB.prefix : client.botconfig.DefaultPrefix
+          }help changelog\` to see all options.`
+        );
+      return message.channel.send(ChangelogEmbed);
+    }
+
     try {
       const changeLogData = fs.readFileSync(
         path.join(__dirname, "..", "changelog.txt"),
@@ -26,11 +58,16 @@ module.exports = {
       );
       let ChangelogEmbed = new MessageEmbed()
         .setColor(client.botconfig.EmbedColor)
-        .setDescription(changeLogData || "No changelog found");
+        .setDescription(changeLogData || "No changelog found")
+        .setFooter(
+          `Type \`${
+            GuildDB ? GuildDB.prefix : client.botconfig.DefaultPrefix
+          }changelog all\` to see all changelog version.`
+        );
       message.channel.send(ChangelogEmbed);
     } catch (err) {
       let ChangelogEmbed = new MessageEmbed()
-        .setColor(client.botconfig.EmbedColor)
+        .setColor("RED")
         .setDescription("Error when load changelog file");
       message.channel.send(ChangelogEmbed);
     }
